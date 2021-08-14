63b7b6b052ba89adfe793a4d8d863abc7c42282d
uaslan <utkuaslan85@gmail.com>
2021-08-14 17:46:53 +0000

    08.14.21

--- untitled1.py
+++ untitled1.py
@@ -1,22 +1,30 @@
 from kafka import KafkaProducer
-from binance_config import *
 from binance import ThreadedWebsocketManager
+import json
 
 # Binance client
 api_key = "wtqoMQp21dTGKL4qrZeHaxv0UZETf55sHOqaWLgu6wAKfmB3N7hTBPjGoMFpHAv3"
 api_secret = "v1YKCDgI11Tl7czwUUgAfBvDWaZjg12hiN3HTMVen8EqDdZuVJxPiPZ3scKW1pBa"
-twm = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret)
 
 # Kafka Client
 producer = KafkaProducer(bootstrap_servers=['kafka.kafka.svc.cluster.local:9092'], value_serializer=lambda m: json.dumps(m).encode('ascii'))
 
-symbol = 'BTCUSD'
 
-def handle_socket_message(msg):
+def main():
 
-    producer.send('test', {symbol : msg})
-    print(msg)
+    symbol = 'BTCUSDT'
+    twm = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret)
 
-twm.start_kline_socket(callback=handle_socket_message, symbol=symbol)
+    twm.start()
 
-twm.join()
+    def handle_socket_message(msg):
+
+        producer.send('test', {symbol : msg})
+
+    twm.start_kline_socket(callback=handle_socket_message, symbol=symbol)
+
+    twm.join()
+
+
+if __name__ == "__main__":
+    main()
